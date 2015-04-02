feea0d634da2cd0950ac46f4a11a315c1b6d2e9c
Charles Oder <charles@oder.us>
2015-04-02 14:47:03 -0500

    Added initial project

---
+++ ProjectEulerSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectEulerSwift
+//
+//  Created by Charles Oder on 4/2/15.
+//  Copyright (c) 2015 Charles Oder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
