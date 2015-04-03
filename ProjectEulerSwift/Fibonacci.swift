4087a79d2485c377842f1088457674d3c340f668
Charles Oder <charles@oder.us>
2015-04-02 20:55:33 -0500

    Problem 2: Completed

--- ProjectEulerSwift/Fibonacci.swift
+++ ProjectEulerSwift/Fibonacci.swift
@@ -10,17 +10,23 @@ import Foundation
 
 public class Fibonacci {
     
-    private var count: Int
+    public var count: Int
+    public var max: Int
+    
+    convenience init(){
+        self.init(count: Int.max)
+    }
     
     init(count: Int) {
         self.count = count
+        self.max = Int.max
     }
     
     public func getSequence() -> [Int]{
         var output: [Int] = []
         var lastTwo = [0, 1]
         var currentValue = lastTwo[1]
-        while (output.count < count ) {
+        while (output.count < count && currentValue <= max) {
             output.append(currentValue)
             lastTwo[0] = lastTwo[1]
             lastTwo[1] = currentValue
--- ProjectEulerSwiftTests/FibonacciTests.swift
+++ ProjectEulerSwiftTests/FibonacciTests.swift
@@ -16,4 +16,21 @@ class FibonacciTests: XCTestCase {
         var fib = Fibonacci(count: 10)
         XCTAssertEqual(expectedArray, fib.getSequence())
     }
+    
+    func testSumOfSequenceUpToFourMillion(){
+        var expectedValue = 4613732 // 4613732
+        var fib = Fibonacci()
+        fib.max = 4000000
+        NSLog("\(Int.max)")
+        var sequence = fib.getSequence()
+        XCTAssertEqual(expectedValue, sequence.reduce(0, combine: sum))
+    }
+    
+    func sum(acc: Int, val:Int) -> Int {
+        if val % 2 == 0 {
+            return val + acc
+        } else {
+            return acc
+        }
+    }
 }
